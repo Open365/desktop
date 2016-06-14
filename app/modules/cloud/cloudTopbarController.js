7d89fe341a2eea18a632cefe329649d8ddd2f864
Franky Armengol <francesc.armengol@eyeos.com>
2016-06-14 11:55:10 +0200

    Move tobarController into new file

---
+++ app/modules/cloud/cloudTopbarController.js
@@ -0,0 +1,43 @@
+/*
+    Copyright (c) 2016 eyeOS
+
+    This file is part of Open365.
+
+    Open365 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+'use strict';
+define([], function () {
+
+    function cloudTobarController ($scope) {
+        var subscriptions =[];
+
+        subscriptions['app.opened'] = window.DesktopBus.subscribe('app.opened', function (data) {
+            $scope.topborderColorClass = 'border-top-' + data.name;
+            $scope.appColorClass = 'color-' + data.name;
+            $scope.iconSvg = 'open365-logo-' + data.name;
+            $scope.topBackgroundColorBeforeClass = 'background-top-' + data.name;
+            $scope.backgroundButtonColor = 'background-button-' + data.name;
+        });
+
+        $scope.$on('$destroy', function() {
+            subscriptions.forEach(function (sub) {
+                sub.unsubscribe();
+            });
+        });
+    }
+
+    return cloudTobarController;
+
+});
